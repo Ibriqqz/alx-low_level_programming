bde56100852796e019f57ad5437c825efff60059
Ibriqqz <irenebriqqz99@gmail.com>
2023-09-09 08:36:04 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
 #!/bin/bash
-int main(void)
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ * */
+int main (void)
+{
+	puts("programming is like building a multilingual puzzle\n");
+	return (0); 
+}
