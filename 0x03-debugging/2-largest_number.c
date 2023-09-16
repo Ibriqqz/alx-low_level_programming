57a6ff82419bdc6ed921ed2cb984c72b2c44d842
Ibriqqz <irenebriqqz99@gmail.com>
2023-09-16 01:06:02 -0700

    largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -2,7 +2,7 @@
 
 /**
 
- *largest_number - returns tge largest of 3 numbers
+ *largest_number - returns the largest of 3 numbers
   * @a: first integer
   * @b: second integer
   * @c: third integer
